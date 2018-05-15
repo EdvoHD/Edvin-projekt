7a2bee876add4747eda64cdb066142025b68ff54
syrgys <edvinaginger@hotmail.com>
2018-05-15 15:18:51 +0200

    Update 4 - väder + clean layout
    darksky api
    Omgjord layout, flyttat på påminnelser
    (kom ihåg push-notiser)
    Mer clean layout
    Väder-ruta koncept klar - nästa steg är att implementera darksky där.

--- Dashboard-Personal/script.js
+++ Dashboard-Personal/script.js
@@ -24,25 +24,24 @@ document.getElementById("date").innerHTML = d.toDateString();
 
 
 $.ajax({
-	url: 'https://api.darksky.net/forecast/f5a32da10816fcae63e298db026df708/37.8267,-122.4233',
+	url: 'https://api.darksky.net/forecast/f5a32da10816fcae63e298db026df708/59.332956,17.980345',
 	method: 'GET',
   headers: {"X-Test-Header": "test-value"},
-	// data: JSON.stringify({var:'val'}), // send data in the request body
-	// contentType: "application/json; charset=utf-8",  // if sending in the request body
-	dataType: 'json'
+  mode: 'no-cors',
+	dataType: 'jsonp'
 }).done(function(data, textStatus, jqXHR) {
-	// because dataType is json 'data' is guaranteed to be an object
 	console.log('done');
-}).fail(function(jqXHR, textStatus, errorThrown) { console.log(jqXHR)
-	// the response is not guaranteed to be json
+}).fail(function(jqXHR, textStatus, errorThrown) {
 	if (jqXHR.responseJSON) {
-		// jqXHR.reseponseJSON is an object
 		console.log('failed with json data');
 	}
 	else {
-		// jqXHR.responseText is not JSON data
 		console.log('failed with unknown data');
 	}
 }).always(function(dataOrjqXHR, textStatus, jqXHRorErrorThrown) {
 	console.log('always');
 });
+
+
+
+// DARKSKY - EGEN
